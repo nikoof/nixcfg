88d36399810f1df113d06fb4a70e9a9fec281a35
Nicolas Bratoveanu <nicolasbratoveanu@gmail.com>
2023-07-30 15:04:27 +0300

    Remove networking from common.nix

--- hosts/common.nix
+++ hosts/common.nix
@@ -3,9 +3,6 @@
 {
   imports = [];
 
-  networking.networkmanager.enable = true;
-  networking.useDHCP = lib.mkDefault true;
-
   time.timeZone = "Europe/Bucharest";
   i18n.defaultLocale = "en_US.UTF-8";
 
