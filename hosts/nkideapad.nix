aa8a744f5870379f9172da28981a0c800343a062
Nicolas Bratoveanu <nicolasbratoveanu@proton.me>
2023-08-05 01:41:22 +0300

    Added power management options for laptop

--- hosts/nkideapad.nix
+++ hosts/nkideapad.nix
@@ -105,6 +105,21 @@
     };
   };
 
+  services.thermald.enable = true;
+  services.auto-cpufreq = {
+    enable = true;
+    settings = {
+      battery = {
+         governor = "powersave";
+         turbo = "never";
+      };
+      charger = {
+         governor = "performance";
+         turbo = "auto";
+      };
+    };
+  };
+
   environment.systemPackages = with pkgs; [
     curl
     neovim
