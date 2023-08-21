f40688187aa13ac27c35914577be39f91976fcee
Nicolas Bratoveanu <nicolasbratoveanu@proton.me>
2023-08-21 14:10:00 +0300

    Remove unused local packages

--- packages/default.nix
+++ packages/default.nix
@@ -3,7 +3,4 @@
 }:
 
 {
-  sddm-sugar-candy-tokyonight = pkgs.libsForQt5.callPackage ./sddm-sugar-candy-tokyonight.nix { };
-  sddm-sugar-candy-tokyonight-nixbg = pkgs.libsForQt5.callPackage ./sddm-sugar-candy-tokyonight-nixbg.nix { };
-  nord-sddm-theme = pkgs.libsForQt5.callPackage ./nord-sddm-theme.nix { };
 }
