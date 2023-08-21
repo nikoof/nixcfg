ffc244caa2c4d6f106b9d02bf7cfa7e851af08db
Nicolas Bratoveanu <nicolasbratoveanu@proton.me>
2023-08-21 13:51:25 +0300

    Fix syncthing id

--- hosts/nkideapad.nix
+++ hosts/nkideapad.nix
@@ -121,7 +121,7 @@
     overrideDevices = true;
     overrideFolders = true;
     devices = {
-      "nkbox" = { id = "6KIED2W-IJFLOZN-BM4KOU3-HOOZFO4-MGZV6LH-Z75QBSY-C3UW73O-2GA3HQO"; };
+      "nkbox" = { id = "WR7JF54-XLCUEQQ-TY2T2AQ-TRRK5U5-MKIU765-ZXDBJHM-APZKZFO-SM6C3QN"; };
       "nkgalaxy" = { id = "FY2JIBO-6VYRLZD-YJBAUSF-W5CMUV7-RCXYVMU-NAKKIHT-NNZLTHA-ZHV3SAE"; };
     };
     folders = {
