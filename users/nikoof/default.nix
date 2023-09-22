92fc36dc41572178b161d2aa1707d1ec8f6542b7
Nicolas Bratoveanu <nicolasbratoveanu@proton.me>
2023-09-22 18:21:41 +0300

    Test out home manager import

--- users/nikoof/default.nix
+++ users/nikoof/default.nix
@@ -1,6 +1,7 @@
-{ config, pkgs , ... }:
+{ config, pkgs, ... }:
 
 {
+  imports = [ ./alacritty.nix ];
   home.username = "nikoof";
   home.homeDirectory = "/home/nikoof";
 
@@ -39,14 +40,13 @@
 
   dconf.settings = {
     "org/virt-manager/virt-manager/connections" = {
-      autoconnect = ["qemu:///system"];
-      uris = ["qemu:///system"];
+      autoconnect = [ "qemu:///system" ];
+      uris = [ "qemu:///system" ];
     };
   };
 
   programs.bash = import ./bash.nix;
   programs.rofi = import ./rofi.nix;
-  programs.alacritty = import ./alacritty.nix;
   programs.starship = import ./starship.nix { inherit pkgs; };
   programs.zathura = import ./zathura.nix;
 
@@ -71,8 +71,8 @@
     enable = true;
     enableAliases = true;
     extraOptions = [
-     "--group-directories-first"
-     "--header"
+      "--group-directories-first"
+      "--header"
     ];
     git = true;
     icons = true;
