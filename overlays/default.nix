432d9bced2ad0c1aa0270ebeaed240374b43a296
Nicolas Bratoveanu <nicolasbratoveanu@proton.me>
2023-09-28 16:09:15 +0300

    Add packet tracer theme fix

--- overlays/default.nix
+++ overlays/default.nix
@@ -7,6 +7,18 @@
     graphite-gtk-theme = prev.graphite-gtk-theme.override {
       tweaks = ["nord"];
     };
+
+    ciscoPacketTracer8 = prev.ciscoPacketTracer8.overrideAttrs {
+      desktopItems = [
+        (prev.makeDesktopItem {
+          name = "cisco-pt8.desktop";
+          desktopName = "Cisco Packet Tracer 8";
+          icon = "ciscoPacketTracer8";
+          exec = "env XDG_CURRENT_DESKTOP=GNOME packettracer8 %f";
+          mimeTypes = ["application/x-pkt" "application/x-pka" "application/x-pkz"];
+        })
+      ];
+    };
   };
 
   unstable-packages = final: prev: {
