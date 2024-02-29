7570739eb2e4995f446f7c5159de1171388d0ff6
Nicolas Bratoveanu <nicolasbratoveanu@proton.me>
2024-02-29 18:26:23 +0200

    add nitrokey mixin

--- hosts/euler/configuration.nix
+++ hosts/euler/configuration.nix
@@ -1,8 +1,9 @@
-{ inputs
-, config
-, pkgs
-, lib
-, ...
+{
+  inputs,
+  config,
+  pkgs,
+  lib,
+  ...
 }: {
   imports = [
     inputs.nixos-hardware.nixosModules.common-pc-laptop
@@ -17,6 +18,8 @@
     ../../mixins/pipewire.nix
     ../../mixins/plasma.nix
 
+    ../../mixins/nitrokey.nix
+
     ./services/power.nix
     ./services/syncthing.nix
 
@@ -27,9 +30,9 @@
   nixpkgs.hostPlatform = "x86_64-linux";
 
   boot.kernelPackages = pkgs.linuxPackages_6_6;
-  boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "vmd" "nvme" "usb_storage" "usbhid" "sd_mod" ];
-  boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-intel" ];
+  boot.initrd.availableKernelModules = ["xhci_pci" "thunderbolt" "vmd" "nvme" "usb_storage" "usbhid" "sd_mod"];
+  boot.initrd.kernelModules = [];
+  boot.kernelModules = ["kvm-intel"];
 
   hardware.bluetooth.enable = true;
   networking = {
@@ -39,30 +42,30 @@
     useDHCP = lib.mkDefault true;
   };
 
-  swapDevices = [{ device = "/swap/swapfile"; }];
+  swapDevices = [{device = "/swap/swapfile";}];
   fileSystems = {
     "/" = {
       device = "/dev/disk/by-uuid/c10e2f83-8c02-4c75-99ab-40aaa3b71bb3";
       fsType = "btrfs";
-      options = [ "subvol=@" ];
+      options = ["subvol=@"];
     };
 
     "/nix" = {
       device = "/dev/disk/by-uuid/c10e2f83-8c02-4c75-99ab-40aaa3b71bb3";
       fsType = "btrfs";
-      options = [ "subvol=@nix" ];
+      options = ["subvol=@nix"];
     };
 
     "/home" = {
       device = "/dev/disk/by-uuid/c10e2f83-8c02-4c75-99ab-40aaa3b71bb3";
       fsType = "btrfs";
-      options = [ "subvol=@home" ];
+      options = ["subvol=@home"];
     };
 
     "/swap" = {
       device = "/dev/disk/by-uuid/c10e2f83-8c02-4c75-99ab-40aaa3b71bb3";
       fsType = "btrfs";
-      options = [ "subvol=@swap" ];
+      options = ["subvol=@swap"];
     };
 
     "/boot" = {
@@ -101,23 +104,9 @@
   ];
 
   services.udev = {
-    packages = [ pkgs.nitrokey-udev-rules ];
-
     # For Pi Pico debug probe (ipw)
     extraRules = ''
       SUBSYSTEM=="usb", ATTR{idVendor}=="2e8a", ATTR{idProduct}=="000c", MODE:="666"
     '';
-  };
-
-  security.pam = {
-    u2f = {
-      enable = true;
-      cue = true;
-      authFile = "/etc/Nitrokey/u2f_keys";
-    };
-    services = {
-      login.u2fAuth = true;
-      sudo.u2fAuth = true;
-    };
   };
 }
--- hosts/gauss/configuration.nix
+++ hosts/gauss/configuration.nix
@@ -18,6 +18,8 @@
     ../../mixins/plasma.nix
     ../../mixins/gns3.nix
 
+    ../../mixins/nitrokey.nix
+
     ./services/syncthing.nix
   ];
 
