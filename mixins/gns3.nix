332071a59c82735b61d39646991267e045828361
Nicolas Bratoveanu <nicolasbratoveanu@proton.me>
2024-03-20 20:14:19 +0200

    fix ubridge for gns3

--- mixins/gns3.nix
+++ mixins/gns3.nix
@@ -5,6 +5,17 @@
   lib,
   ...
 }: {
+  virtualisation.libvirtd.enable = true;
+
+  users.groups.ubridge = {};
+  security.wrappers.ubridge = {
+    source = "/run/current-system/sw/bin/ubridge";
+    capabilities = "cap_net_admin,cap_net_raw=ep";
+    owner = "root";
+    group = "ubridge";
+    permissions = "u+rx,g+x";
+  };
+
   environment.systemPackages = with pkgs; [
     gns3-server
     gns3-gui
