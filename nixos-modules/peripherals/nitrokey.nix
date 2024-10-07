e0f9c688db62ab6715d6364eb469741f1f135ad6
Nicolas Bratoveanu <nicolasbratoveanu@proton.me>
2024-10-08 01:14:56 +0200

    small fixes

--- nixos-modules/peripherals/nitrokey.nix
+++ nixos-modules/peripherals/nitrokey.nix
@@ -20,21 +20,21 @@ in {
       environment.systemPackages = with pkgs; [
         pinentry
         pinentry-qt
+
+        python3Packages.pynitrokey
       ];
 
       security.pam = {
         u2f = {
           enable = true;
           cue = true;
+          control = "sufficient";
           authFile = "/etc/Nitrokey/u2f_keys";
         };
 
         services = {
           login.u2fAuth = true;
           sudo.u2fAuth = true;
-          kde.u2fAuth = true;
-          sddm.u2fAuth = true;
-          polkit-1.u2fAuth = true;
         };
       };
     })
